e690b5532b4ed5ff1493366726a106026705c6de
Mariusz Jurgielewicz <mordor@mail.com>
2025-01-04 14:12:19 -0800

    HD44780 / 1602A LCD

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # rust-rpicow-discovery
 Embedded Rust with Raspberry Pi Pico(w)
 
+## Pico Pinout
+[Pico Pinout](https://pico.pinout.xyz/)
 
 ## Examples
 This project uses probe-run to run the examples. Once set up, it should be as simple as `cargo run --example <example name> --release`.
@@ -9,4 +11,19 @@ This project uses probe-run to run the examples. Once set up, it should be as si
 
 ![DHT11](images/pico_dht11_bb.png)
 
+### HD44780 / 1602A LCD
+
+HD44780 | RPi Pico | Function
+--------|-------------|---------
+1 (Vss) | Ground (0V) | Powers the HD44780's processor
+2 (Vdd) | 5V | Powers the HD44780's processor
+3 (V0) | Ground (0V) via a 1K resistor | LCD contrast control: high for nothing, low for aggressive white squares that hate you
+4 (RS) | Pi Pico GP15 (pin 20) | Register Select. High to send data, low to send instructions (like 'clear screen').
+5 (R/W) | Ground (0V) | Read/write toggle. I've kept it permanently tied low since we only ever output to the display.
+6 (E) | Pi Pico GP14 (pin 19) | Enable signal.
+11, 12, 13, 14 (DB4-DB7) | GP18, GP19, GP20, GP21 (pins 24-27) | Data pins.
+15 (BLA) | 5V | Backlight LED anode.
+16 (BLK) | Ground (0V) via 220 ohm resistor| Backlight LED cathode.
+
+![HD44780](images/pico_hd44780_bb.png)
 
---
+++ examples/hd44780.rs
@@ -0,0 +1,89 @@
+//! # LCD Display Example
+//!
+//! In this example, the RP2040 is configured to drive a small two-line
+//! alphanumeric LCD using the
+//! [HD44780](https://crates.io/crates/hd44780-driver) driver.
+//!
+//! It drives the LCD by pushing data out of six GPIO pins. It may need to be
+//! adapted to your particular board layout and/or pin assignment.
+
+
+#![no_std]
+#![no_main]
+
+use bsp::entry;
+use defmt_rtt as _;
+use panic_probe as _;
+
+use bsp::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    sio::Sio,
+    watchdog::Watchdog,
+};
+
+use rp_pico as bsp;
+use hd44780_driver as hd44780;
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
+
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // Create the LCD driver from some GPIO pins
+    let mut lcd = hd44780::HD44780::new_4bit(
+        pins.gpio15.into_push_pull_output(), // Register Select
+        pins.gpio14.into_push_pull_output(), // Enable
+        pins.gpio18.into_push_pull_output(), // d4
+        pins.gpio19.into_push_pull_output(), // d5
+        pins.gpio20.into_push_pull_output(), // d6
+        pins.gpio21.into_push_pull_output(), // d7
+        &mut delay,
+    )
+    .unwrap();
+
+    // Clear the screen
+    lcd.reset(&mut delay).unwrap();
+    lcd.clear(&mut delay).unwrap();
+
+    // Write to the top line
+    lcd.write_str("Pico on", &mut delay).unwrap();
+
+    // Move the cursor
+    lcd.set_cursor_pos(40, &mut delay).unwrap();
+
+    // Write more more text
+    lcd.write_str("HD44780!", &mut delay).unwrap();
+
+    // Do nothing - we're finished
+    loop {
+        cortex_m::asm::wfi();
+    }
+}
+
+// End of file
